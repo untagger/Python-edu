f67ebbd57b72143026fdc1bad98199eaac1845ba
untagger <145845359+untagger@users.noreply.github.com>
2024-04-25 12:36:00 +0300

    Delete 019.py

--- 019.py
+++
@@ -1,7 +0,0 @@
-import re
-a = input("Введите номер автомобиля РФ формата Е001КХ777: ")
-b = re.compile(r'[А-Я]\d{3}[А-Я]{2}\d{2,3}')
-if re.match(b,a):
-    print("Верно")
-else:
-    print("Не верно")
